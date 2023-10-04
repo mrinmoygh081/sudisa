74471c70be1572eddd0bede1fab8fea4f18f3127
mrinmoygh081 <mrinmoygh081@gmail.com>
2023-10-04 14:53:34 +0530

    db update for deployment:

--- sudisa.sql
+++ sudisa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 6.0.0-dev+20230711.6a6929c361
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Sep 08, 2023 at 04:30 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.5
+-- Host: 127.0.0.1
+-- Generation Time: Oct 04, 2023 at 11:15 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,8 +33,23 @@ CREATE TABLE `alarm` (
   `camera` varchar(12) NOT NULL,
   `alarm_type` varchar(15) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `datetime` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `datetime` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `alarm`
+--
+
+INSERT INTO `alarm` (`alarm_id`, `dept_id`, `camera`, `alarm_type`, `image`, `datetime`) VALUES
+(85, 19, 'Camera 10', 'Warning', '1696331065707-frame553e534b-2bc2-4c3c-9d4d-708b1d34ca57.jpg', '2023-10-03T16:34:25.755Z'),
+(86, 19, 'Camera 10', 'Warning', '1696331443844-frame572d87c5-c142-438e-9a7f-db259cda7ed0.jpg', '2023-10-03T16:40:43.915Z'),
+(87, 19, 'Camera 10', 'Warning', '1696331460994-frameb74cca17-128d-4d3c-8f18-0af605811ef6.jpg', '2023-10-03T16:41:01.042Z'),
+(88, 19, 'Camera 10', 'Warning', '1696331573884-framee914ba54-b211-4b13-ab1b-b584f1257aae.jpg', '2023-10-02T16:42:53.982Z'),
+(89, 30, 'Cam 1', 'Warning', '1696403194026-download.jpg', '2023-10-04T12:37:02.228Z'),
+(90, 19, 'Camera 10', 'Warning', '1696405621735-frame0480449c-cbaa-4f57-a40e-a2870c93237b.jpg', '2023-10-04T13:17:01.786Z'),
+(91, 19, 'Camera 10', 'Warning', '1696407149501-framedec11982-1d1c-45a8-8e0a-86e1773c18aa.jpg', '2023-10-04T13:42:29.582Z'),
+(92, 19, 'Camera 10', 'Warning', '1696407151194-framef9bae2d8-b7ea-4c86-b153-d8cbbbcfe695.jpg', '2023-10-04T13:42:31.275Z'),
+(93, 19, 'Camera 10', 'Warning', '1696408859986-framebfc0532f-b832-472d-93b1-80c13a18353d.jpg', '2023-10-04T14:11:00.081Z');
 
 -- --------------------------------------------------------
 
@@ -48,7 +63,7 @@ CREATE TABLE `auth` (
   `password` varchar(255) NOT NULL,
   `name` varchar(55) NOT NULL,
   `isActive` varchar(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `auth`
@@ -68,14 +83,16 @@ CREATE TABLE `depts` (
   `dept_name` varchar(45) NOT NULL,
   `emails` varchar(255) NOT NULL,
   `head_name` varchar(65) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `depts`
 --
 
 INSERT INTO `depts` (`dept_id`, `dept_name`, `emails`, `head_name`) VALUES
-(8, 'Manufacturing Department', 'Hello', 'Mrinmoy Ghosh');
+(19, 'Manufacturing', 'upasanabharti145@gmail.com,upasanabharti146@gmail.com', 'Upasana '),
+(25, 'System', 'mrinmoygh081@gmail.com', 'Mrinmoy Ghosh'),
+(30, 'IT', 'mrinmoygh081@gmail.com,upasanabharti145@gmail.com', 'Mrinmoy');
 
 --
 -- Indexes for dumped tables
@@ -107,7 +124,7 @@ ALTER TABLE `depts`
 -- AUTO_INCREMENT for table `alarm`
 --
 ALTER TABLE `alarm`
-  MODIFY `alarm_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `alarm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- AUTO_INCREMENT for table `auth`
@@ -119,7 +136,7 @@ ALTER TABLE `auth`
 -- AUTO_INCREMENT for table `depts`
 --
 ALTER TABLE `depts`
-  MODIFY `dept_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `dept_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
